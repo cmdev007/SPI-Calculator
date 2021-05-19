18940a1056bc318dabca39234606ef8baba3f6ee
Preet Patel <preet.makz@gmail.com>
2021-05-19 17:04:57 +0530

    gradCalc: Added leader board

--- gradeCalc.py
+++ gradeCalc.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import datetime
 import time
 from load_css import local_css
 import os, sqlite3
+import pandas as pd
 
 st.set_page_config(
     page_title="SPI:Calc",
@@ -13,6 +13,37 @@ st.set_page_config(
 
 local_css("style.css")
 
+############### SQLITTE ###############
+
+def create_table():
+    conn = sqlite3.connect('spidata.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS spilog(unix REAL, uname TEXT, spi TEXT, sem TEXT)")
+    c.close()
+    conn.close()
+
+
+def data_entry(unix, uname, spi, sem):
+    uname = uname.replace("'", "''")
+    conn = sqlite3.connect('spidata.db')
+    c = conn.cursor()
+    c.execute(f"INSERT INTO spilog VALUES('{unix}', '{uname}', '{spi}', '{sem}')")
+    conn.commit()
+    c.close()
+    conn.close()
+
+def read_data(sem):
+    conn = sqlite3.connect('spidata.db')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM spilog WHERE sem = '{sem}'")
+    data = c.fetchall()
+    return data
+
+############## SQLITE ################
+
+if "spidata.db" not in os.listdir():
+    create_table()
+
 placeTitle = st.empty()
 placeTitle.markdown(f"<div style='font-size: 50px;;color:grey;font-family:orbitron;'><center><b>SPI Calculator</b></center></div>", unsafe_allow_html=True)
 st.markdown(f"<div style='font-size: 12px;'><center>By <a href='https://github.com/cmdev007/'><span class='highlight blue'><span class='bold'>cMDev007</span></span></a></center></div>", unsafe_allow_html=True)
@@ -21,19 +52,20 @@ SEM = st.sidebar.radio("",("Semester-2","Semester-1"))
 if SEM == "Semester-2":
     app_state = st.experimental_get_query_params()
     if app_state == {}:
-        with st.form(key='columns_in_form'):
+        with st.beta_expander("SPI Calculator",expanded=True):
+            with st.form(key='columns_in_form'):
 
-            nInput = st.text_input("Your name please (keep empty if want to hide identity)")
+                nInput = st.text_input("Your name please (keep empty if want to hide identity)")
 
-            elec = st.selectbox("Technical Elective", ("","Information Retrieval","No SQL Databases","Advanced Image Processing","Multimedia Security and Forensics"))
+                elec = st.selectbox("Technical Elective", ("","Information Retrieval","No SQL Databases","Advanced Image Processing","Multimedia Security and Forensics"))
 
-            submitted = st.form_submit_button('Submit')
-            if submitted:
-                if nInput.strip()=="":
-                    nInput = "anonymous"
-                else:
-                    nInput = nInput.lower().strip()
-                st.experimental_set_query_params(**{"nInput": nInput, "elec": elec})
+                submitted = st.form_submit_button('Submit')
+                if submitted:
+                    if nInput.strip()=="":
+                        nInput = "anonymous"
+                    else:
+                        nInput = nInput.lower().strip()
+                    st.experimental_set_query_params(**{"nInput": nInput, "elec": elec})
     app_state = st.experimental_get_query_params()
     if app_state != {}:
         placeTitle.markdown(f"<div style='font-size:30px;color:grey;font-family:orbitron;'>\
@@ -70,10 +102,24 @@ if SEM == "Semester-2":
             if not MFleg:
                 SPI = str(round((Numerator*10/Denominator),2))
                 st.markdown(f"<div style='font-size: 16px;'><center>Your SPI: <span class='highlight green'><span class='bold'>{SPI}</span></span></center></div>", unsafe_allow_html=True)
+                data_entry(int(time.time()), nInput, SPI, SEM)
             else:
                 st.markdown(f"<div style='font-size: 16px;'><center>Please select grades of <span class='highlight green'><span class='bold'>All Subjects!</span></span></center></div>", unsafe_allow_html=True)
-
-
+            st.markdown("---")
+    # Leaderboard
+    with st.beta_expander("Leader Board", expanded=False):
+        # st.markdown(
+        #     f"<div style='font-size: 20px;'><center><span class='highlight green'><span class='bold'>Coming Soon!</span></span></center></div>",
+        #     unsafe_allow_html=True)
+        allSPI = []
+        for i in read_data(SEM):
+            allSPI.append(float(i[2]))
+        allSPI.sort(reverse=True)
+        allName = [f"Classmate {i+1}" for i in range(len(allSPI))]
+        st.write(pd.DataFrame({
+        'Anonymous Name': allName,
+        'SPI': allSPI,
+        }))
 if SEM == "Semester-1":
     st.markdown(
         f"<div style='font-size: 20px;'><center><span class='highlight green'><span class='bold'>Coming Soon!</span></span></center></div>",
